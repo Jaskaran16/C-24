6c8416ef17a76a6b6efff3d4e65fdf897c8df6f8
Jaskaran16 <69311941+Jaskaran16@users.noreply.github.com>
2020-08-27 13:38:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var trashbin,trashbin2,trashbin3;
+var ground;
+var trash;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(600,height,1200,20)
+	trash = new Trash(660,40)
+	trashbin = new Trashbin(650,685,100,10)
+	trashbin2 = new Trashbin(600,650,10,100)
+	trashbin3 = new Trashbin(700,650,10,100)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background(0);
+  trash.display()
+  trashbin.display()
+  trashbin2.display()
+  trashbin3.display()
+  ground.display()
+}
+
+
+
